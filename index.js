7fdf032922625b5353af71d2b7e9d2e0937100fb
Mehdi Dissem <mehdissem@gmail.com>
2023-03-02 15:41:27 +0100

    generated second key

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const { Configuration, OpenAIApi } = require("openai");
 require('dotenv').config()
 
+//starting config
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  // adding async completion fn
+  async function runCompletion () {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "How are you today?",
+    });
+    console.log(completion.data.choices[0].text);
+    }
+    
+    runCompletion();
